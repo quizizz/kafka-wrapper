57404b18df0ac974a75719fc5007ab45de45feac
Anas Khan <anas@quizizz.com>
2022-03-10 12:52:54 +0530

    index.ts creation

---
+++ lib/index.d.ts
@@ -0,0 +1,4 @@
+import getKafkaProducer from './producer';
+import getKafkaConsumer from './consumer';
+import KafkaAdmin from './admin';
+export { getKafkaConsumer, getKafkaProducer, KafkaAdmin, };
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import getKafkaProducer from './producer'
+import getKafkaConsumer from './consumer';
+import KafkaAdmin from './admin';
+
+export {
+  getKafkaConsumer,
+  getKafkaProducer,
+  KafkaAdmin,
+}
